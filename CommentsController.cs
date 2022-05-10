306aa2c01da27e65ea8b0f5c3a5fcb7eeec0e1f4
CherieAurelius <101117752+CherieAurelius@users.noreply.github.com>
2022-05-10 21:33:13 +0200

    created the submit function  at controller, now working  on  commentlistPartial

--- CommentsController.cs
+++ CommentsController.cs
@@ -16,7 +16,26 @@ namespace Connected.Controllers
         [HttpPost]
         public ActionResult SubmitComment(CommentModel model)
         {
-            
+            if(!ModelState.IsValid){
+                returm CurrentUmbracoPage();
+            }
+            else
+            {
+                int userId = 0;
+                var content = Current.Services.ContentService.Create(model.Title, CurrentPage.Id, "comment",userId );
+
+                content.SetValue("title", model.Title);
+                content.SetValue("creator", model.Name);
+                content.SetValue("email", model.Email);
+                content.SetValue("comments", model.Comments);
+
+                Umbraco.Core.Service.OperationResult  saveResult =  Current.Services.ContentService.Save(content);
+
+                if(saveResult.Result  == 0 )
+                    return RedirectToCurrentUmbracoPage();
+            }
+        
+         return RedirectToCurrentUmbracoPage();
 
         }
     }
